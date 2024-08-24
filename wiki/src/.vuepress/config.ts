8521f4354910b2c7c96202bbca9074da974a75b0
EDGW <3538722943@qq.com>
2024-08-24 22:03:24 +0800

    Changed base route

--- wiki/src/.vuepress/config.ts
+++ wiki/src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/cs-public-wiki/",
+  base: "/",
 
   lang: "zh-CN",
   title: "主页",
